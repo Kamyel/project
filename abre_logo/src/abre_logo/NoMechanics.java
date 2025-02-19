317a705fb6195f40f9caa43237b0cb674305baa9
Kamy com Y <kamyelgi@gmail.com>
2025-02-18 22:10:23 -0300

    Update NoMechanics.java

--- abre_logo/src/abre_logo/NoMechanics.java
+++ abre_logo/src/abre_logo/NoMechanics.java
@@ -1,12 +1,1 @@
-package abre_logo;
 
-public class NoMechanics {
-	
-	public static int initialX = 390, initialY = 250;
-	public static boolean isPressed = false;
-	
-	public NoMechanics() {
-		
-	}
-
-}
